e0cbf458a3b2941261d7253c253091340a918324
Stu Forbes <mr.stu.forbes@gmail.com>
2013-10-21 11:33:27 +0100

    Failing to test

--- spec/steps/test/steps.js
+++ spec/steps/test/steps.js
@@ -2,6 +2,7 @@ stepDefinitions('Sample step definitions', function(){
 
   step('Call an external step with \'([^"]*)\' and value \'([^"]*)\'', function(param, value){
     console.log('Invoked step \'Call an external step with \''+param+'\' and value \''+value+'\'');
+//    expect(true).toBe(false);
   });
 
   step('Do something else', function(){
