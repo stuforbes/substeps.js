b1e067513af85baef3c63edc8df310521cb68c2d
Stu Forbes <mr.stu.forbes@gmail.com>
2013-10-22 10:50:44 +0100

    Adding expectation capability to step impls; validation around step impl structure

--- spec/steps/test/steps.js
+++ spec/steps/test/steps.js
@@ -1,11 +1,12 @@
 stepDefinitions('Sample step definitions', function(){
 
   step('Call an external step with \'([^"]*)\' and value \'([^"]*)\'', function(param, value){
-    console.log('Invoked step \'Call an external step with \''+param+'\' and value \''+value+'\'');
-//    expect(true).toBe(false);
+//    console.log('Invoked step \'Call an external step with \''+param+'\' and value \''+value+'\'');
+
+    expect(value % 2).toBe(0);
   });
 
   step('Do something else', function(){
-    console.log('Invoked step \'Do something else');
+//    console.log('Invoked step \'Do something else');
   });
 });
