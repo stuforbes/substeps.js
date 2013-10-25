aa9be59b08edde49867697b00238dc96eea71368
Stu Forbes <mr.stu.forbes@gmail.com>
2013-10-25 11:23:15 +0100

    Minor enhancements; cleaning up package.json

--- spec/steps/test/steps.js
+++ spec/steps/test/steps.js
@@ -1,12 +1,16 @@
 stepImplementations('Sample step implementations', function(){
 
+  var _;
+
   step('Call an external step with \'([^"]*)\' and value \'([^"]*)\'', function(param, value){
 //    console.log('Invoked step \'Call an external step with \''+param+'\' and value \''+value+'\'');
 
     expect(value % 2).toBe(0);
+    _ = require('underscore');
   });
 
   step('Do something else', function(){
 //    console.log('Invoked step \'Do something else');
+//    console.log(_.filter([1, 2, 3, 4], function(item){ return item % 2 === 0; }));
   });
 });
