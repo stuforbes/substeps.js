3dd5922f7bd6cf513ae0a0f703a16812119b7560
Stu Forbes <mr.stu.forbes@gmail.com>
2013-11-21 14:22:03 +0000

    Updating executionFactory to pass callback into step impl executions

--- spec/steps/test/steps.js
+++ spec/steps/test/steps.js
@@ -2,15 +2,19 @@ stepImplementations('Sample step implementations', function(){
 
   var _;
 
-  step('Call an external step with \'([^"]*)\' and value \'([^"]*)\'', function(param, value){
+  step('Call an external step with \'([^"]*)\' and value \'([^"]*)\'', function(param, value, callback){
 //    console.log('Invoked step \'Call an external step with \''+param+'\' and value \''+value+'\'');
 
-    expect(value % 2).toBe(0);
-    _ = require('underscore');
+//    expect(value % 2).toBe(0);
+//    _ = require('underscore');
+
+    callback();
   });
 
-  step('Do something else', function(){
+  step('Do something else', function(callback){
 //    console.log('Invoked step \'Do something else');
 //    console.log(_.filter([1, 2, 3, 4], function(item){ return item % 2 === 0; }));
+
+    callback();
   });
 });
